54063e33bf42562d28700e820eb3734ddfbfa60e
JohannEOsnaya <jjesuso1400@alumno.ipn.mx>
2023-05-02 03:11:22 -0600

    Se agregó funcionalidad y conexion con la base de datos, así como los metodos DELETE y PUT

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
-app.use(express.json())
-app.use(cors())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method: ', request.method)
+    console.log('Path:   ', request.path)
+    console.log('Body:   ', request.body)
+    console.log('---')
+    next()
+}
+
+
 app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
+app.use(cors())
 
+/*
 let notes = [
     {
         id: 1,
@@ -23,33 +36,36 @@ let notes = [
         content: "Soy SIMP",
         important: false
     }
-]
+]*/
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => note.id === id)
-
-    if(note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+        if(note) {
+            response.json(note)
+        } else {
+            response.status(500).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const genereteId = () => {
@@ -62,22 +78,57 @@ const genereteId = () => {
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if(!body.content) {
+    if(body.content === undefined) {
         return response.status(404).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: genereteId(),
-    }
+    })
 
-    notes = notes.concat(note)
-    response.json(notes)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
+app.put('/api/notes/:id', (request, respose, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params, note, {new: true})
+    .then(updatedNote => {
+        respose.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
